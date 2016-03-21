f5371c2e97a40dc7a98f5d363db4b9ab0e0e464c
Norbert Melzer <timmelzer@gmail.com>
2016-03-21 15:06:49 +0100

    Set development version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exygen.Mixfile do
 
   def project do
     [app: :exygen,
-     version: "0.0.1",
+     version: "0.0.1-dev",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
